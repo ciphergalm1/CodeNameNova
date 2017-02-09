29ce1fcbb35b81eb4310e97a4330057c7c21f4f3
nada <nada>
2017-02-09 20:33:50 +0900

    update enemy spawn

--- Source/CodeNameNuggets/FighterPawn.cpp
+++ Source/CodeNameNuggets/FighterPawn.cpp
@@ -359,14 +359,14 @@ void AFighterPawn::SearchTarget()
 		//DrawDebugLine(GetWorld(), StartPos, EndPos, FColor::Green, false, 10.f);
 		if (bHasDetecTarget) {
 			for (auto it = HitResults.CreateIterator(); it; it++) {
-				if ((*it).GetActor() != this) {
-					FVector targetLocation = (*it).Actor->GetActorLocation();
-					FString targetName = "Find target: " + (*it).Component->GetName();
+				if ((*it).GetActor() != this && (*it).GetActor()->IsA(AEnemyPawn::StaticClass())) {
+					//FVector targetLocation = (*it).Actor->GetActorLocation();
+					//FString targetName = "Find target: " + (*it).Component->GetName();
 					//DrawDebugSphere(GetWorld(), targetLocation, 500.f, 16, FColor::Green, false, 10.f);
 					if ((*it).Actor->GetRootComponent()->ComponentHasTag(FName("EnemyAircraft"))) {
-						GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Target Accquired!"));
-						FString message = "Target Accquired: " + (*it).Actor->GetName();
-						GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, message);
+						//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Target Accquired!"));
+						//FString message = "Target Accquired: " + (*it).Actor->GetName();
+						//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, message);
 						AActor* target = (*it).GetActor();
 						targetSelected = Cast<AEnemyPawn>(target);
 						
@@ -482,7 +482,7 @@ void AFighterPawn::ClearSelectTarget()
 {
 	if (targetSelected) {
 		targetSelected->SetLockOnStatus(0);
-		LockOnGauge = 0;
+		LockOnGauge = 0.f;
 		targetSelected = nullptr;
 	}
 }
